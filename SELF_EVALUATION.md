f7533384cac6d37495e34f5eb9052a6d37c25882
mscott716 <mscott4@macalester.edu>
2025-09-23 15:48:58 -0500

    almost done!
    my emojis just stopped showing up after working

--- SELF_EVALUATION.md
+++ SELF_EVALUATION.md
@@ -4,9 +4,9 @@
 
 ### Correctness
 
-- [ ] Image background shows trees 60% of the time, and mountains 50% of the time (mountains and trees may both appear in one image)
-- [ ] The program draws a “family” of emojis, containing a number of emojis based on the parameters to `createFamily()`
-- [ ] Each emoji in the family is randomly selected, based on your implementation of `createRandomEmoji()`
+- [x] Image background shows trees 60% of the time, and mountains 50% of the time (mountains and trees may both appear in one image)
+- [x] The program draws a “family” of emojis, containing a number of emojis based on the parameters to `createFamily()`
+- [x] Each emoji in the family is randomly selected, based on your implementation of `createRandomEmoji()`
 - [ ] When run, your program iterates through an endless slideshow of these emoji photos, each with randomized backgrounds and emoji styles
   - [ ] There is a pause for three seconds on each image so that a viewer may enjoy the slideshow!
 - [ ] (Optional) Additional randomly-selected background scenery
@@ -18,27 +18,27 @@
 
 Check these items from the [Comp 127 Style Guide](https://comp127.innig.net/resources/style-guide/):
 
-- [ ] all classes are in packages
-- [ ] package names start with a lowercase letter
-- [ ] newly created Java files have a header comment with
+- [x] all classes are in packages
+- [x] package names start with a lowercase letter
+- [x] newly created Java files have a header comment with
     - [ ] author name
     - [ ] brief description of class, and
     - [ ] acknowledgement, if appropriate
 - [ ] identifier (variable and method) names are descriptive
-- [ ] variable and method names are in lowerCamelCase (no CapitalizedNames,
+- [x] variable and method names are in lowerCamelCase (no CapitalizedNames,
   names_with_underscores)
-- [ ] class names are singular nouns
-- [ ] class names are in UpperCamelCase
-- [ ] proper indentation:
-    - [ ] opening curly braces (“{”) are at the end of the line
-    - [ ] closing curly braces (“}”) are on their own line
-    - [ ] the indentation of closing braces is the same as the indentation of the
+- [x] class names are singular nouns
+- [x] class names are in UpperCamelCase
+- [x] proper indentation:
+    - [x] opening curly braces (“{”) are at the end of the line
+    - [x] closing curly braces (“}”) are on their own line
+    - [x] the indentation of closing braces is the same as the indentation of the
       opening statements they match
-    - [ ] lines are indented according to how deeply they are nested
-- [ ] completed TODOs are deleted
-- [ ] extra blank lines are deleted
-- [ ] unneeded commented lines of code are deleted
-- [ ] print statements used for testing are deleted
+    - [x] lines are indented according to how deeply they are nested
+- [x] completed TODOs are deleted
+- [x] extra blank lines are deleted
+- [x] unneeded commented lines of code are deleted
+- [x] print statements used for testing are deleted
 
 
 ## Reflection
@@ -48,11 +48,13 @@ sentence or two in response to each question is plenty.
 
 **What did you miss? What did you wish you did better?**
 
-
+I don't think I missed anything, but I wish when
 
 **What challenges did you face, and how did you overcome them?**
 
+I was challenged by the positionFamily method. To overcome this I drew out the positions of the emojis and how the variables being passed into positinFamily are being used. I eventually realized that I was not adding the total amount of x distance covered to each emoji and without a cummulation variable, the positions would not be accurate.
 
+**What is something that was interesting or exciting, or a lesson you learned from this experience that you want to remember?**
 
-**What is something that was interesting or exciting, or a lesson you learned
-  from this experience that you want to remember?**
+I thought it was interesting to interact with the random class. Coding random variables is somewhat similar to Python, but after learning how classes and class methods work, I understand the syntax of using the random class better.
+
--- src/emojivacation/EmojiVacation.java
+++ src/emojivacation/EmojiVacation.java
@@ -18,7 +18,16 @@ public class EmojiVacation {
         TREE_LEAVES_COLOR = new Color(0x17af13),
         GRASS_COLOR = new Color(0xbcda9f),
         MOUNTAIN_COLOR = new Color(0x769afe),
-        NO_SLIDE_COLOR = new Color(0x22211a);
+        NO_SLIDE_COLOR = new Color(0x22211a),
+        HEAD_COLOR = new Color(0xFFDE30),
+        HEAD_OUTLINE_COLOR = new Color(0xAC9620),
+        MOUTH_COLOR = new Color(0xE45B5B),
+        IRIS_COLOR = new Color(0x294CD6),
+        PUPIL_COLOR = new Color(0x000307),
+        OCULAR_HIGHLIGHT_COLOR = new Color(0xD4FFFFFF, true),
+        EYELID_COLOR = new Color(0x3E1919),
+        NAUSEOUS_COLOR = new Color(0x589817),
+        NAUSEOUS_LINE_COLOR = new Color(0x0F1A04);
 
     private static final int
         SCENE_WIDTH = 800,
@@ -32,8 +41,15 @@ public class EmojiVacation {
     }
 
     private static void doSlideShow(CanvasWindow canvas) {
-        // TODO: [Instructions step 8] Change this to an actual slideshow
+
         generateVacationPhoto(canvas);
+        // while (true) {
+        //     generateVacationPhoto(canvas);
+        //     canvas.draw();
+        //     canvas.pause(3000);
+        //     canvas.removeAll();
+        // }
+
     }
 
     private static void generateVacationPhoto(CanvasWindow canvas) {
@@ -43,19 +59,29 @@ public class EmojiVacation {
 
         addCloudRows(canvas);
 
-        // TODO: [Instructions step 2] Create mountains 50% of the time.
-        //       You should randomly determine the size and number of layers
-        //       (within reasonable constraints).
+        // Adds mountains 50% of the time
+        if (percentChance(50)) {
+            double mtnSize = random.nextDouble(120, 200);
+            int mtnLayer = random.nextInt(1,5);
+
+            addMountains(canvas, 400, mtnSize, mtnLayer);
+        }
 
         addGround(canvas, 400);
 
-        // TODO: [Instructions step 2] Create forests 60% of the time. You should randomly
-        //       determine the count for the number of trees. Pick reasonable values for
-        //       other parameters.
+        // Adds trees 60% of the time
+        if (percentChance(60)) {
+            int count = random.nextInt(1,30);
+
+            addForest(canvas, 400, 800, count);
+        }
 
         List<GraphicsGroup> family = createFamily(2, 3);
         positionFamily(family, 60, 550, 20);
-        // TODO: [Instructions step 4] Add each emoji in the list to the canvas
+
+        for (GraphicsGroup emoji : family) {
+            canvas.add(emoji);
+        }
     }
 
     // –––––– Emoji family –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -63,27 +89,41 @@ public class EmojiVacation {
     private static List<GraphicsGroup> createFamily(int adultCount, int childCount) {
         double adultSize = 160, childSize = 90;
 
-        // TODO: [Instructions step 6] Change this so that instead of always creating one adult
-        //       and one child, it instead creates a list containing adultCount adults,
-        //       and childCount children.
-        //
-        // Hint: You can't use List.of() to do this, because you don't know the size of the
-        // resulting list before the code actually runs. What can you use?
-        //
-        return List.of(
-            createRandomEmoji(adultSize),
-            createRandomEmoji(childSize));
+        List<GraphicsGroup> family = new ArrayList <> ();
+
+        for (int i = 0; i < adultCount; i++) {
+            family.add(createRandomEmoji(adultSize));
+        }
+
+        for (int i = 0; i < childCount; i++) {
+            family.add(createRandomEmoji(childSize));
+        }
+
+        return family;
     }
 
     private static GraphicsGroup createRandomEmoji(double size) {
-        // TODO: [Instructions step 7] Change this so that instead of always creating a smiley face,
-        //       it randomly selects one of the many available emojis.
-        //
-        // Hint: You can use chained if/else conditionals: with a certain probability, return emoji
-        // type A, else with some other probability return emoji type B, else with a certain
-        // probability ... etc ... else return a smiley by default.
-        //
-        return ProvidedEmojis.createSmileyFace(size);
+
+        GraphicsGroup emoji = createSmileyFace(size);
+
+        int face = random.nextInt(1,5);
+
+        for (int i = 1; i <= face; i++) {
+            if (i == 1) {
+                emoji = createSmileyFace(size);
+            } else if (i == 2) {
+                emoji = createWinkingFace(size);
+            } else if (i == 3) {
+                emoji = createFrownyFace(size);
+            } else if (i == 4) {
+                emoji = createNauseousFace(size);
+            } else if (i == 5) {
+                emoji = createContentedFace(size);
+            }
+        }
+
+        return emoji;
+
     }
 
     private static void positionFamily(
@@ -92,15 +132,20 @@ public class EmojiVacation {
             double baselineY,
             double spacing
     ) {
-        // TODO: [Instructions step 5] Iterate over the emojis in the list,
-        //       and position them all in a neat row
 
-        // The leftmost emoji’s left edge should be at leftX, and spacing is the number of pixels that should be between
-        // each emoji and the next. But how to you space them if the kids and adults have different widths? (Hint: you
-        // can ask any graphics object for its width.)
-        //
-        // The bottom of each emoji should be baselineY. But setPosition() sets the _top_! How do you set the bottom to
-        // a given position? (Hint: you can ask any graphics object for its height.)
+        double totalX = leftX;
+
+        for (GraphicsGroup emoji : family) {
+            double width = emoji.getWidth();
+            double height = emoji.getHeight();
+
+            double x = totalX;
+            double y = height - baselineY;
+
+            emoji.setPosition(x, y);
+
+            totalX = totalX + spacing + width;
+        }
     }
 
     // –––––– Scenery ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -373,5 +418,183 @@ public class EmojiVacation {
      */
     private static int colorChannelVariation(int c, int amount) {
         return Math.min(255, Math.max(0, c + randomInt(-amount, amount)));
+    }
+
+    //-------------------------------------------------------------------------
+
+    /*
+     * Emojis
+     */
+
+     /**
+     * Creates a smiley face emoji.
+     *
+     * @param size The overall width and height of the emoji.
+     * @return A graphic that you can add to a window, or place inside some other graphics group.
+     */
+    public static GraphicsGroup createSmileyFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        group.add(createHead(size, size));
+
+        GraphicsGroup leftEye = createEye(size * 0.15);
+        leftEye.setCenter(size * 0.3, size * 0.3);
+        group.add(leftEye);
+
+        GraphicsGroup rightEye = createEye(size * 0.15);
+        rightEye.setCenter(size * 0.7, size * 0.3);
+        group.add(rightEye);
+
+        Arc mouth = createSmile(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.75);
+        group.add(mouth);
+
+        return group;
+    }
+
+    public static GraphicsGroup createFrownyFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        group.add(createHead(size, size));
+
+        GraphicsGroup leftEye = createEye(size * 0.15);
+        leftEye.setCenter(size * 0.3, size * 0.3);
+        group.add(leftEye);
+
+        GraphicsGroup rightEye = createEye(size * 0.15);
+        rightEye.setCenter(size * 0.7, size * 0.3);
+        group.add(rightEye);
+
+        Arc mouth = createFrown(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.7);
+        group.add(mouth);
+
+        return group;
+    }
+
+    public static GraphicsGroup createWinkingFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        group.add(createHead(size, size));
+
+        GraphicsGroup leftEye = createEye(size * 0.15);
+        leftEye.setCenter(size * 0.3, size * 0.3);
+        group.add(leftEye);
+
+        Line rightEye = createFlatEyelid(size * 0.15, 0);
+        rightEye.setCenter(size * 0.7, size * 0.3);
+        group.add(rightEye);
+
+        Arc mouth = createSmile(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.75);
+        group.add(mouth);
+
+        return group;
+    }
+
+    public static GraphicsGroup createContentedFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        group.add(createHead(size, size));
+
+        Arc leftEye = createClosedEye(size * 0.15);
+        leftEye.setCenter(size * 0.3, size * 0.3);
+        group.add(leftEye);
+
+        Arc rightEye = createClosedEye(size * 0.15);
+        rightEye.setCenter(size * 0.7, size * 0.3);
+        group.add(rightEye);
+
+        Arc mouth = createSmile(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.75);
+        group.add(mouth);
+
+        return group;
+    }
+
+    public static GraphicsGroup createNauseousFace(double size) {
+        GraphicsGroup group = new GraphicsGroup();
+
+        Ellipse head = createHead(size, size);
+        head.setFillColor(NAUSEOUS_COLOR);
+        head.setStrokeColor(NAUSEOUS_LINE_COLOR);
+        group.add(head);
+
+        Line leftEye = createFlatEyelid(size * 0.15, -0.1);
+        leftEye.setCenter(size * 0.3, size * 0.3);
+        leftEye.setStrokeColor(NAUSEOUS_LINE_COLOR);
+        group.add(leftEye);
+
+        Line rightEye = createFlatEyelid(size * 0.15, 0.1);
+        rightEye.setCenter(size * 0.7, size * 0.3);
+        rightEye.setStrokeColor(NAUSEOUS_LINE_COLOR);
+        group.add(rightEye);
+
+        Arc mouth = createFrown(size * 0.6, size * 0.5);
+        mouth.setCenter(size * 0.5, size * 0.75);
+        mouth.setStrokeColor(NAUSEOUS_LINE_COLOR);
+        group.add(mouth);
+
+        return group;
+    }
+
+    private static GraphicsGroup createEye(double size) {
+        GraphicsGroup eye = new GraphicsGroup();
+
+        Ellipse eyeBall = new Ellipse(0, 0, size, size);
+        eyeBall.setFillColor(PUPIL_COLOR);
+        eyeBall.setStrokeColor(IRIS_COLOR);
+        eyeBall.setStrokeWidth((float) (size * 0.2));
+        eye.add(eyeBall);
+
+        Ellipse ocularHighlight = new Ellipse(size * 0.5, size * 0.1, size * 0.4, size * 0.4);
+        ocularHighlight.setFillColor(OCULAR_HIGHLIGHT_COLOR);
+        ocularHighlight.setStroked(false);
+        eye.add(ocularHighlight);
+
+        return eye;
+    }
+
+    private static Arc createClosedEye(double size) {
+        Arc eyelid = new Arc(0, 0, size * 1.2, size, -20, -140);
+        eyelid.setStrokeWidth((float) (size * 0.12));
+        eyelid.setStrokeColor(EYELID_COLOR);
+        return eyelid;
+    }
+
+    private static Line createFlatEyelid(double width, double slope) {
+        Line eye = new Line(0, 0, width, width * slope);
+        eye.setStrokeWidth((float) (width * 0.15));
+        eye.setStrokeColor(EYELID_COLOR);
+        return eye;
+    }
+
+    private static Ellipse createHead(double height, double width) {
+        Ellipse head = new Ellipse(0, 0, width, height);
+        head.setFillColor(HEAD_COLOR);
+        head.setStrokeColor(HEAD_OUTLINE_COLOR);
+        head.setStrokeWidth(2);
+        return head;
+    }
+
+    private static Arc createSmile(double ellipseWidth, double ellipseHeight) {
+        Arc mouth = new Arc(0, 0, ellipseWidth, ellipseHeight, 200, 140);
+        mouth.setStrokeColor(MOUTH_COLOR);
+        mouth.setStrokeWidth(4);
+        return mouth;
+    }
+
+    private static Line createFlatMouth(double width) {
+        Line mouth = new Line(0, 0, width, 0);
+        mouth.setStrokeColor(MOUTH_COLOR);
+        mouth.setStrokeWidth((float) (width * 0.1));
+        return mouth;
+    }
+
+    private static Arc createFrown(double ellipseWidth, double ellipseHeight) {
+        Arc mouth = new Arc(0, 0, ellipseWidth, ellipseHeight, 20, 140);
+        mouth.setStrokeColor(MOUTH_COLOR);
+        mouth.setStrokeWidth(4);
+        return mouth;
     }
 }
